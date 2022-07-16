f75cd61ec7c7470d8e006c86dbf0348da45a6539
PogorzelskiP <PogorzelskiP@outlook.com>
2022-07-16 15:17:22 +0200

    DODANIE KLASY NOWYCH KLAS W PROGRAMIE CABRIO
    Dodanie swóch klas z funkcjami odpowiadającymi za składanie i
    otwieranie dachu

--- JavaStart/ObjectPrograming1/Cabrio/Cabrio.java
+++ JavaStart/ObjectPrograming1/Cabrio/Cabrio.java
@@ -1,22 +1,78 @@
 package JavaStart.ObjectPrograming1.Cabrio;
 
 public class Cabrio {
-    String name;
-    String manufacturer;
-    int topSpeed;
-    boolean roof;
+    private String name;
+    private String manufacturer;
+    private int topSpeed;
+    private boolean roof, isDriving;
 
-    public Cabrio(String name, String manufacturer) {
-        this.name = name;
-        this.manufacturer = manufacturer;
-    }
-
-    public Cabrio(String name, String manufacturer, int topSpeed, boolean roof) {
+    public Cabrio(String name, String manufacturer, int topSpeed, boolean roof, boolean isDriving) {
         this.name = name;
         this.manufacturer = manufacturer;
         this.topSpeed = topSpeed;
         this.roof = roof;
+        this.isDriving = isDriving;
     }
 
-    
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    public int getTopSpeed() {
+        return topSpeed;
+    }
+
+    public void setTopSpeed(int topSpeed) {
+        this.topSpeed = topSpeed;
+    }
+
+    public boolean isRoof() {
+        return roof;
+    }
+
+    public void setRoof(boolean roof) {
+        this.roof = roof;
+    }
+
+    public boolean isDriving() {
+        return isDriving;
+    }
+
+    public void setDriving(boolean driving) {
+        isDriving = driving;
+    }
+
+    public void openRoof(boolean isDriving){
+        if(isDriving){
+            System.out.println("Car is moving! You can't open the roof!");
+        }else if(roof){
+                System.out.println("Roof is already open!");
+        }else{
+            setRoof(true);
+            System.out.println("Roof is open");
+        }
+    }
+
+    public void closeRoof(boolean isDriving){
+        if(isDriving){
+            System.out.println("Car is moving! You can't close the roof!");
+        }else if (!roof){
+                System.out.println("Roof is already close!");
+        }else{
+            setRoof(false);
+            System.out.println("Roof is closed!");
+        }
+    }
 }
--- JavaStart/ObjectPrograming1/Cabrio/main.java
+++ JavaStart/ObjectPrograming1/Cabrio/main.java
@@ -2,6 +2,7 @@ package JavaStart.ObjectPrograming1.Cabrio;
 
 public class main {
     public static void main(String[] args) {
-
+        Cabrio cabrio = new Cabrio("Passat","Volkswagen", 340, false, false);
+        cabrio.openRoof(cabrio.isDriving());
     }
 }
