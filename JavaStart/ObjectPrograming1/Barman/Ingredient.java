bf24c5c58234a9ae7ccec1ed3ad3d3d3df7eeb20
PogorzelskiP <PogorzelskiP@outlook.com>
2022-07-17 11:07:44 +0200

    Class ingredient is done.

--- JavaStart/ObjectPrograming1/Barman/Ingredient.java
+++ JavaStart/ObjectPrograming1/Barman/Ingredient.java
@@ -1,4 +1,26 @@
 package JavaStart.ObjectPrograming1.Barman;
-
 public class Ingredient {
+    private String name;
+    private int amount;
+
+    public Ingredient(String name, int amount) {
+        this.name = name;
+        this.amount = amount;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
 }
