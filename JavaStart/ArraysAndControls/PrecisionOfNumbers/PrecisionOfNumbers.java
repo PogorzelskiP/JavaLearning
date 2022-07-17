4241593c9783a4be7d7a50373198e66e9074f3ae
PogorzelskiP <PogorzelskiP@outlook.com>
2022-07-17 19:25:02 +0200

    ADD FOLDER PRECISION OF NUMBERS
    Added folder precision of numbers

---
+++ JavaStart/ArraysAndControls/PrecisionOfNumbers/PrecisionOfNumbers.java
@@ -0,0 +1,12 @@
+package JavaStart.ArraysAndControls.PrecisionOfNumbers;
+
+import java.util.Scanner;
+
+public class PrecisionOfNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Podaj liczbe całkowitą różną od zera");
+        double x = scanner.nextDouble();
+        
+    }
+}
