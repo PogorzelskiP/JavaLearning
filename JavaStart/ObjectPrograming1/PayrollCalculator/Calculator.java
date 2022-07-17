5b79117a831deedab283310d76c76e076e2fc13c
PogorzelskiP <PogorzelskiP@outlook.com>
2022-07-17 12:08:42 +0200

    ADDED TWO METHODS
    I added two methods in Calculator class.

--- JavaStart/ObjectPrograming1/PayrollCalculator/Calculator.java
+++ JavaStart/ObjectPrograming1/PayrollCalculator/Calculator.java
@@ -1,4 +1,11 @@
 package JavaStart.ObjectPrograming1.PayrollCalculator;
-
 public class Calculator {
+    public static double nettoYearPayment(Employee employee){
+        
+    }
+
+    public static double bruttoYearPayment(Employee employee){
+
+    }
+
 }
