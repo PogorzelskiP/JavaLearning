9696927528e396e20501fc2407a8246a2e872716
PogorzelskiP <PogorzelskiP@outlook.com>
2022-07-17 12:18:51 +0200

    TESTING PAYROLL CALCULATOR
    I'm testing payroll calculator

--- JavaStart/ObjectPrograming1/PayrollCalculator/Calculator.java
+++ JavaStart/ObjectPrograming1/PayrollCalculator/Calculator.java
@@ -1,11 +1,10 @@
 package JavaStart.ObjectPrograming1.PayrollCalculator;
 public class Calculator {
     public static double nettoYearPayment(Employee employee){
-        
+        return employee.getSalary()*12;
     }
 
     public static double bruttoYearPayment(Employee employee){
-
+        return ((Calculator.nettoYearPayment(employee)*1.2d)+(12*1000));
     }
-
 }
--- JavaStart/ObjectPrograming1/PayrollCalculator/CalculatorTest.java
+++ JavaStart/ObjectPrograming1/PayrollCalculator/CalculatorTest.java
@@ -2,6 +2,8 @@ package JavaStart.ObjectPrograming1.PayrollCalculator;
 
 public class CalculatorTest {
     public static void main(String[] args) {
-
+        Employee employee = new Employee("Paweł","Pogorzelski",3000);
+        System.out.println("Dochód netto pracownika wynosi: " + Calculator.nettoYearPayment(employee));
+        System.out.println("Dochód brutto pracownika wynosi: " + Calculator.bruttoYearPayment(employee));
     }
 }
