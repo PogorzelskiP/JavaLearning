5ad24150627d45ed95446294311a2038a7bbb7de
PogorzelskiP <PogorzelskiP@outlook.com>
2022-07-17 11:45:35 +0200

    TESTING APP
    I created object Drink and used createDrink and printDrink functions.

--- JavaStart/ObjectPrograming1/Barman/Barman.java
+++ JavaStart/ObjectPrograming1/Barman/Barman.java
@@ -4,7 +4,7 @@ public class Barman {
         return new Drink(ingredient1,ingredient2,ingredient3);
     }
     public static void printDrink(Drink drink){
-        System.out.printf("Składniki drinka to %s %s %s w proporcjach %d, %d, %d, jego pojemność to: %d ml",drink.getIngredient1().getName(),drink.getIngredient2().getName(),
+        System.out.printf("Składniki drinka to: %s, %s, %s, w proporcjach: %d, %d, %d, jego pojemność to: %d ml",drink.getIngredient1().getName(),drink.getIngredient2().getName(),
         drink.getIngredient3().getName(),drink.getIngredient1().getAmount(),drink.getIngredient2().getAmount(),drink.getIngredient3().getAmount(),
                 (drink.getIngredient1().getAmount()+drink.getIngredient2().getAmount()+drink.getIngredient3().getAmount()));
     }
--- JavaStart/ObjectPrograming1/Barman/BarmanTest.java
+++ JavaStart/ObjectPrograming1/Barman/BarmanTest.java
@@ -2,6 +2,8 @@ package JavaStart.ObjectPrograming1.Barman;
 
 public class BarmanTest {
     public static void main(String[] args) {
-        
+        Drink drink = Barman.createDrink(new Ingredient("Vodka",200),new Ingredient("Milk",200),
+                new Ingredient("Syrup",50));
+        Barman.printDrink(drink);
     }
 }
