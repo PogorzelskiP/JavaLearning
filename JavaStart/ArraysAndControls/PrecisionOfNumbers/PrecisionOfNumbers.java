836c97e0c166f542b23dd677766c36136d48545b
PogorzelskiP <PogorzelskiP@outlook.com>
2022-07-18 19:59:42 +0200

    ADDED NEW CLASS NUMBERS PRECISION

--- JavaStart/ArraysAndControls/PrecisionOfNumbers/PrecisionOfNumbers.java
+++ JavaStart/ArraysAndControls/PrecisionOfNumbers/PrecisionOfNumbers.java
@@ -1,12 +1,20 @@
 package JavaStart.ArraysAndControls.PrecisionOfNumbers;
 
 import java.util.Scanner;
-
 public class PrecisionOfNumbers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Podaj liczbe całkowitą różną od zera");
-        double x = scanner.nextDouble();
-        
+        float a = scanner.nextFloat();
+
+        if (a > 0) {
+            for (float x = 0; x <= a + 0.1; x += 0.1) {
+                System.out.printf("%.1f\n", x);
+            }
+        } else if (a < 0) {
+            for (float x = 0; x >= a - 0.1; x -= 0.1) {
+                System.out.printf("%.1f\n", x);
+            }
+        }
     }
 }
